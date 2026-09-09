1a74e0042dea926b4de8d28703b1e22ef397793c
Varvara Bondareva <BondareVarechka@yandex.ru>
2026-09-09 19:46:48 +0300

    fix: 1 select

--- lab_02/select.sql
+++ lab_02/select.sql
@@ -1,3 +1,5 @@
 -- 1. select с предикатом сравнения
+-- Ученики класса с id = 3, у которых есть справки об отсутствии по причине "карантин"
+SELECT S.first_name, S.last_name, A.reason, A.absent_date FROM students S JOIN absents AS A ON S.id = A.student_id WHERE S.class_group_id = 3 AND A.reason = 'Карантин'; 
 
-SELECT S.first_name, S.last_name, A.absent_date FROM students S JOIN absents AS A ON S.id = A.student_id WHERE A.reason = 'Уважительная' AND S.class_group_id = 3; 
+-- 2. select c предикатом BETWEEN
